22c0e6f004967940bcb87fc3ce0e4943c6391794
Ankit-Infotech2025 <ankit.kumawat@maitriiinfotech.com>
2025-07-18 15:10:39 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import NavigationDots from './Component/NavigationDots'; // Assuming this component exists
 import { AppRoutes } from './routes/index'; // Import your route constants
-import { AiOutlineProduct } from 'react-icons/ai';
-
 
 // Import-icons/ai";t all screen components
 import SplashScreen from './pages/SplashScreen';
